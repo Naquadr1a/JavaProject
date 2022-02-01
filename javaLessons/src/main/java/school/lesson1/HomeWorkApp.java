0728d7dfb9291aee5248f2ce3e2b379d760ccc69
Naquadr1a <1naquadria1@gmail.com>
2022-02-01 21:47:25 +0300

    112

--- javaLessons/src/main/java/school/lesson1/HomeWorkApp.java
+++ javaLessons/src/main/java/school/lesson1/HomeWorkApp.java
@@ -3,5 +3,10 @@ package school.lesson1;
 public class HomeWorkApp {
     public static void main(String[] args) {
         System.out.println("WeeWee");
+        System.out.println("WeeWee");
+        System.out.println("WeeWee");
+        System.out.println("WeeWee");
+        System.out.println("WeeWee");
+        System.out.println("WeeWee");
     }
 }
