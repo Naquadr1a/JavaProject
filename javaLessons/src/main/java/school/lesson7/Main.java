70211d554c7a5024c794b206e9a9aa8d1dc33337
Naquadr1a <1naquadria1@gmail.com>
2022-02-24 02:04:46 +0300

    The solution to the seventh home task. Addition.

--- javaLessons/src/main/java/school/lesson7/Main.java
+++ javaLessons/src/main/java/school/lesson7/Main.java
@@ -30,11 +30,11 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-    try (PrintWriter writer = new PrintWriter("file.csv")) {
+    public static void main(String[] args) throws IOException {
+    try (PrintWriter writer = new PrintWriter(new FileWriter("file.csv", true))) {
         StringBuffer string = new StringBuffer();
 
-        //Создаем и передаем данные в файл file.csv
+        //Передаем данные в файл file.csv
         string.append("Value1");
         string.append(";");
         string.append("Value2");
@@ -52,7 +52,6 @@ public class Main {
         string.append(3000);
         string.append(";");
         string.append(3000);
-        string.append(";");
         string.append("\n");
 
         string.append(4000);
@@ -62,16 +61,25 @@ public class Main {
         string.append(6000);
         string.append(";");
         string.append(3000);
+        string.append("\n");
+
+        string.append(7000);
+        string.append(";");
+        string.append(8000);
+        string.append(";");
+        string.append(9000);
+        string.append(";");
+        string.append(1000);
         string.append(";");
         string.append("\n");
 
-        string.append(4000);
+        string.append(7000);
         string.append(";");
-        string.append(5000);
+        string.append(8000);
         string.append(";");
-        string.append(6000);
+        string.append(9000);
         string.append(";");
-        string.append(3000);
+        string.append(1000);
         string.append(";");
         string.append("\n");
 
@@ -82,9 +90,10 @@ public class Main {
         e.printStackTrace();
     }
 
-    //Создаем обьект класса Appdata, и запускаем метод для прочитки файла
+        //Создаем обьект класса Appdata, и запускаем метод для прочитки, и очистки файла
     AppData appdata = new AppData();
     appdata.readFile();
+    appdata.save();
     }
 }
 
@@ -93,39 +102,40 @@ class AppData {
     int[][] data;
 
     void readFile(){
-        //Переносим значения из файла в ArrayList
+        //Читаем файл file.csv
         try (Scanner scanner = new Scanner(new File("file.csv"))) {
             List<List<String>> values = new ArrayList<>();
             while (scanner.hasNextLine()) {
                 values.add(getRecords(scanner.nextLine()));
-                header = new String[values.get(0).size()];
-                data = new int[values.size()][values.get(0).size()];
             }
 
+            //ставим соответствующую длинну для массивов header и data
+            header = new String[values.get(0).size()];
+            data = new int[values.size()][values.get(0).size()];
+
+            //Заполняем массивы header и data данными из ArrayList
             for (int i = 0; i < header.length; i++) {
                 header[i] = values.get(0).get(i);
-//                data[0][i] = Integer.parseInt(values.get(1).get(i));
-//                data[1][i] = Integer.parseInt(values.get(1).get(i));
             }
 
-            for (int i = 0; i < data.length; i++) {
-                for (int j = 1; j < data[i].length; j++) {
-                    data[j][i] = Integer.parseInt(values.get(j).get(i));
+            for (int i = 1; i < values.size(); i++) {
+                for (int j = 0; j < values.get(0).size(); j++) {
+                    data[i - 1][j] = Integer.parseInt(values.get(i).get(j));
                 }
             }
 
             //Отображаем в консоль содержание массивов header и data
-            System.out.println("header: " + Arrays.toString(header));
-            for (int i = 1; i < data.length; i++) {
-                System.out.println("data[" + i + "]: " + Arrays.toString(data[i]));
-
+            System.out.println("Содержимое массива header: " + Arrays.toString(header));
+            for (int i = 0; i < values.size() - 1; i++) {
+                System.out.println("Содержимое массива data[" + i + "]: " + Arrays.toString(data[i]));
             }
-            System.out.println(values);
+
+//            System.out.println(values);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
-
+        //Записываем полученные данные в ArrayList
         List<String> getRecords(String line) {
             List<String> values = new ArrayList<>();
             try (Scanner scan = new Scanner(line)) {
@@ -135,5 +145,14 @@ class AppData {
                 }
             }
             return values;
+    }
+
+    // Метод для очистки файла file.csv (не разобрался как сделать согласно заданию)
+    void save() throws IOException {
+        FileWriter clear = new FileWriter("file.csv", false);
+        PrintWriter print = new PrintWriter(clear, false);
+        print.flush();
+        print.close();
+        clear.close();
     }
 }
